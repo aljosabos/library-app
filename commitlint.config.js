a313edb57cdf74bf3ff5b238b92a20ec19e5be89
Aljosa Boskovic <aljosabos@gmail.com>
2025-02-25 21:22:51 +0100

    chore: Update code

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,11 +4,7 @@ const Configuration = {
    * Referenced packages must be installed
    */
   extends: ["@commitlint/config-conventional"],
-  /*
-   * Resolve and load conventional-changelog-atom from node_modules.
-   * Referenced packages must be installed
-   */
-  parserPreset: "conventional-changelog-atom",
+
   /*
    * Resolve and load @commitlint/format from node_modules.
    * Referenced package must be installed
@@ -18,21 +14,15 @@ const Configuration = {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    "type-enum": [2, "always", ["foo"]],
+    "type-enum": [
+      2,
+      "always",
+      ["chore", "fix", "refactor", "test", "style", "feat"],
+    ],
+
+    "subject-case": [2, "always", ["sentence-case"]],
+    "header-max-length": [2, "always", 50],
   },
-  /*
-   * Array of functions that return true if commitlint should ignore the given message.
-   * Given array is merged with predefined functions, which consist of matchers like:
-   *
-   * - 'Merge pull request', 'Merge X into Y' or 'Merge branch X'
-   * - 'Revert X'
-   * - 'v1.2.3' (ie semver matcher)
-   * - 'Automatic merge X' or 'Auto-merged X into Y'
-   *
-   * To see full list, check https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/is-ignored/src/defaults.ts.
-   * To disable those ignores and run rules always, set `defaultIgnores: false` as shown below.
-   */
-  ignores: [(commit) => commit === ""],
   /*
    * Whether commitlint uses the default ignore rules, see the description above.
    */
@@ -42,17 +32,6 @@ const Configuration = {
    */
   helpUrl:
     "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
-  /*
-   * Custom prompt configs
-   */
-  prompt: {
-    messages: {},
-    questions: {
-      type: {
-        description: "please input type:",
-      },
-    },
-  },
 };
 
-export default Configuration;
+module.exports = Configuration;
---
+++ server/eslint.config.mjs
@@ -0,0 +1,52 @@
+import pluginJs from "@eslint/js";
+import typescriptParserPlugin from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    plugins: {
+      import: importPlugin,
+      prettier: prettierPlugin,
+      typescriptParser: typescriptParserPlugin,
+    },
+    rules: {
+      complexity: ["error", 2],
+
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern:
+                "{@components,@utils,@constants,@hooks,@styles,@actions,@public,@api,@store,@context}{,/**}",
+              group: "internal",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          warnOnUnassignedImports: true,
+        },
+      ],
+    },
+  },
+];
