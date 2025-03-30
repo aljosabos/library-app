b75b841f3f1f507a091827c2f8622fc9c7b778aa
Aljosa Boskovic <aljosabos@gmail.com>
2025-03-30 13:44:29 +0200

    chore: Add filter books on BE

--- client/src/components/features/SearchBook/SearchBook.tsx
+++ client/src/components/features/SearchBook/SearchBook.tsx
@@ -2,11 +2,18 @@
 
 import { useForm } from "react-hook-form";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Typography } from "@components/Typography/Typography";
+import { Button } from "@components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,52 +22,70 @@ import {
 import { Input } from "@components/ui/input";
 
 export const SearchBook = () => {
-  const form = useForm();
+  const form = useForm({
+    defaultValues: {
+      filter: "title",
+      search: "",
+    },
+  });
+
+  const handleFormSubmit = () => console.log("submit");
 
   return (
     <div>
-      <Typography className="bg-red-400" variant="h1">
+      <Typography variant="h1" className="mb-2">
         Search book
       </Typography>
-      <Typography variant="h3">Search book</Typography>
-
-      <Typography variant="small">Search book</Typography>
 
       <Form {...form}>
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                c This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <form
+          className="flex flex-col gap-6"
+          onSubmit={form.handleSubmit(handleFormSubmit)}
+        >
+          <div className="flex items-end gap-4">
+            <FormField
+              control={form.control}
+              name="filter"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Search by</FormLabel>
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="min-w-[150px]">
+                        <SelectValue placeholder="Search book by" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="title">Title</SelectItem>
+                      <SelectItem value="author">Author</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Author</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            <FormField
+              control={form.control}
+              name="search"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Search" {...field} autoComplete="off" />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button>Search</Button>
+          </div>
+        </form>
       </Form>
+
+      <div className="mt-6 h-auto border border-gray-100"></div>
     </div>
   );
 };
--- server/types.ts
+++ server/types.ts
@@ -1,3 +1,8 @@
 export interface CustomError extends Error {
   statusCode?: number;
 }
+
+export interface ISearchBookParams {
+  search?: string;
+  filter: string;
+}
