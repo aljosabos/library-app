748105e9c92d1df3c4d4041d198b7637cdc5d058
Aljosa Boskovic <aljosabos@gmail.com>
2025-05-07 17:14:33 +0200

    chore: Update code

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -57,6 +57,7 @@ export const loginUser = async (req: Request, res: Response) => {
     expires: new Date(Date.now() + oneDay),
     sameSite: "none",
     secure: true,
+    path: "/",
   });
 
   res.status(StatusCodes.OK).json({ user });
