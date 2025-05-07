2a7b62488414dcb9ee54553280853c5de2703cc2
Aljosa Boskovic <aljosabos@gmail.com>
2025-05-07 10:51:46 +0200

    chore: Update env

--- client/src/api/user/get-current.ts
+++ client/src/api/user/get-current.ts
@@ -15,14 +15,17 @@ export const getCurrentUser = async (): Promise<
   console.log(cookie);
 
   try {
-    const response = await fetch(`${process.env.BASE_URL}/users/current`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: cookie,
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/users/current`,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: cookie,
+        },
       },
-    });
+    );
     const data = await response.json();
 
     return data?.user;
